40fd528cdeaafc8b5923bac77a2f13e050993ad6
OnicarN <ebaudani@gmail.com>
2026-03-23 21:46:52 +0100

    Adding Authentication

--- app/src/main/java/com/example/wibuchat100/HelperClass.java
+++ app/src/main/java/com/example/wibuchat100/HelperClass.java
@@ -1,38 +1,24 @@
 package com.example.wibuchat100;
 
 public class HelperClass {
+    private String uid;
     private String username;
     private String email;
 
-    private String password;
+    public HelperClass() {} // Constructor vacío obligatorio para Firebase
 
-    public HelperClass(String username, String email, String password) {
+    public HelperClass(String uid, String username, String email) {
+        this.uid = uid;
         this.username = username;
         this.email = email;
-        this.password = password;
     }
 
-    public String getUsername() {
-        return username;
-    }
+    public String getUid() { return uid; }
+    public void setUid(String uid) { this.uid = uid; }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 }
